0f185f2aa5fc5d5003917a02247b0255890b882e
Maria <you@example.com>
2020-04-13 16:42:33 +0700

    init

--- freelansim.js
+++ freelansim.js
@@ -15,14 +15,14 @@ async function getItem(url) {
     return {
         id: task_id = url.split('/').pop(),
         title: document.querySelector(".task__title").textContent,
-        desc: document.querySelector(".task__description").textContent.replace('\n', ''),
+        desc: document.querySelector(".task__description").textContent.trim().replace("\n", " "),
         tags: tags,
-        date_in: document.querySelector(".task__meta").textContent.replace('\n', '').split('•')[0],
-        response: document.querySelector(".task__meta").textContent.replace('\n', '').split('•')[1],
-        view: document.querySelector(".task__meta").textContent.replace('\n', '').split('•')[2],
+        date_in: document.querySelector(".task__meta").textContent.split('•')[0].trim(),
+        response: document.querySelector(".task__meta").textContent.split('•')[1].trim().replace("\n", "").replace("\nотклик", "отклик").replace("\nотклика", "отклика").replace("\nоткликов", "откликов"),
+        view: document.querySelector(".task__meta").textContent.split('•')[2].trim().replace("\n", "").replace("\nпросмотр", "просмотр").replace("\nпросмотра", "просмотра").replace("\nпросмотров", "просмотров"),
         user_id: document.querySelector(".fullname a").attributes.href.value.split('/')[2],
         user: document.querySelector(".fullname a").textContent,
-        feedbacks: document.querySelector(".user_statistics").childNodes[11].textContent.replace('\n', ''),
+        feedbacks: document.querySelector(".user_statistics").childNodes[11].textContent.trim().replace("\n", " "),
         
     }
 }
@@ -42,6 +42,11 @@ async function getData() {
         const task = new JSDOM(taskHTML).window.document
         const title = task.querySelector(".task__title a").innerHTML;
         const link = 'https://freelance.habr.com/' + task.querySelector(".task__title a").attributes.href.value;
+        
+        let urgent
+        const urgentHTML = task.querySelector(".task__urgent");
+        if (urgentHTML) { 
+            urgent = urgentHTML.textContent }
 
         const { id, desc, tags, date_in, response, view, user_id, user, feedbacks } = await getItem(link);
 
@@ -53,12 +58,11 @@ async function getData() {
         if (priceHTML) {
             const prices = priceHTML.innerHTML.split(/ <span class="suffix">/);
             price_value = Number.parseInt(prices[0].replace(' ', ''))
-            price_valuta = prices[0].split(" ").pop()
-            price_type = prices[1]
-
+            price_valuta = prices[0].split(" ").pop();
+            price_type = prices[1].replace("проект</span>", "проект").replace("час</span>", "час")
         }
 
-        result.push({ title, price_value, price_type, price_valuta, desc, date_in, response, view, tags, user_id, user, feedbacks })
+        result.push({ id, title, urgent, price_value, price_type, price_valuta, desc, date_in, response, view, tags, user_id, user, feedbacks })
     }
 
     return result;
