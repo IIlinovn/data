321317592b08f28a5fdf0e5f98b78bc20bf2ef6d
ILINOV NICOLAY <you@example.com>
2020-04-14 16:32:36 +0700

    demo Как подключить еще один файл

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const fs = require("fs");
 let freelansim = require('./freelansim')
+let freelansim2 = require('./freelansim2')
 
 function send(result) {
     fs.writeFileSync('result.json', JSON.stringify(result, 2, 2))
 }
 
 freelansim(false, send);
+
+//freelansim2(false, send)
